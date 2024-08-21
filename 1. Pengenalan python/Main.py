e8a735b9a26ed179d626d312471647368d020408
Muhammad Rizal <fbi.rizal3@gmail.com>
2024-08-21 20:17:33 +0700

    Update folder and file

---
+++ 1. Pengenalan python/Main.py
@@ -0,0 +1,35 @@
+# Memulai program pertama kali menggunakan python
+print('Hello World')
+
+# Tutorial membuat komentar atau dokumentasi didalam kode program
+print('Hello Comment')
+
+# Ini adalah comment one line
+
+'''
+    Ini adalah comment multiline
+'''
+
+# Meng-compile python ke bytecode
+'''
+    Pada terminal yang ada kita masukkan command
+
+        python -m py_compile <'Nama File.py'>
+
+    Jika ingin menjalankan program nya
+
+        cd __pycache__
+
+        python Main.cpython-312.pyc
+
+    Fungsi ketika kita melakukan compile ke bytecode, berfungsi untuk melakukan pemrosesan dengan sangat cepat karna kita meng-compile ke bahasa kode program seperti penggunaan C++ ke file .exe
+'''
+
+# Alur program
+
+'''
+    Alur program python dilakukan dengan sangat baik dan python bisa dilakukan interpreter atau ke bytecode yang mudah dipahami bahasa mesin, yang nantinya bisa melakukan pemrosesan dengan sangat baik.
+
+
+    Secara alur python proses pertama dilakukan adalah Developer melakukan penulisan kode program, lalu dilanjutkan ke interpreter/Penerjemah program dan langsung dieksekusi tanpa harus melalui proses compile. Akan tetapi python juga mendukung proses dimana kita mengubah be bahasa bytecode yang mudah dipahami oleh bahasa mesin sehingga dilakukan proses compile.
+'''
